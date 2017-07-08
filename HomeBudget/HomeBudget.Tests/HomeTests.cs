953c710546acb8df3d544a7dbd155bf95cc23f1e
Tomasz Jurkowski <jurek993@outlook.com>
2017-07-08 16:38:41 +0200

    Initial commit

---
+++ HomeBudget/HomeBudget.Tests/HomeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomeBudget.Tests
+{
+    [TestClass]
+    public class HomeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
